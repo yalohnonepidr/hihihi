b46e64a229a993acc45476dea61f42c24e466cc5
yalohnonepidr <yalohnonepidrr@gmail.com>
2024-02-17 00:59:05 +0300

    pochemu

---
+++ Assets/EnemyGoblin/Scripts/EnemyAI.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> patrolPoints;
+    public PlayerController player;
+    public float viewAngle;
+
+    private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitComponentLinks();
+        PickNewPatrolPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NoticePlayerUpdate();
+        ChaseUpdate();
+        patrolUpdate();
+    }
+
+
+
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+    private void InitComponentLinks()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+    }
+    private void patrolUpdate()
+    {
+        if(!_isPlayerNoticed)
+        {
+            if (_navMeshAgent.remainingDistance==0)
+            {
+            PickNewPatrolPoint();
+            }
+        }
+
+    } 
+    private void NoticePlayerUpdate()
+    {
+        var direction = player.transform.position - transform.position;
+        _isPlayerNoticed=false;
+        if (Vector3.Angle(transform.forward, direction)<viewAngle)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(transform.position+Vector3.up, direction, out hit))
+            {
+                if(hit.collider.gameObject == player.gameObject)
+                {
+                _isPlayerNoticed=true;
+                }
+            }
+        }
+    }
+    private void ChaseUpdate()
+    {
+        if(_isPlayerNoticed)
+        {
+           _navMeshAgent.destination = player.transform.position; 
+        }
+    }
+}
---
+++ Assets/Player/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float gravity = 9.8f;
+    public float jumpForce;
+    public float speed;
+
+    private float _fallVelocity = 0;
+    private CharacterController _characterController;
+    private Vector3 _moveVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController=GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveVectorUpdate();
+        JumpUpdate();
+    }
+    void FixedUpdate()
+    {
+        moveFixedUpdate();
+        jumpFixedUpdate();
+    }
+
+
+    private void moveVectorUpdate()
+    {
+        _moveVector = Vector3.zero;
+
+        if (Input.GetKey(KeyCode.W)){
+            _moveVector += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S)){
+            _moveVector -= transform.forward;
+        }
+        if (Input.GetKey(KeyCode.D)){
+            _moveVector += transform.right;
+        }
+        if (Input.GetKey(KeyCode.A)){
+            _moveVector -= transform.right;
+        }
+    }
+    private void JumpUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        {
+            _fallVelocity = - jumpForce;
+        }
+    }
+    private void moveFixedUpdate()
+    {
+        _characterController.Move(_moveVector*speed*Time.fixedDeltaTime);
+    }
+    private void jumpFixedUpdate()
+    {
+        _fallVelocity += gravity*Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+        if(_characterController.isGrounded)
+        {
+            _fallVelocity = 0;
+        }
+    }
+}
