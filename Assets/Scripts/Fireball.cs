7dbcdbb664b561037d3da3e86478fc53297bc4e3
yalohnonepidr <yalohnonepidrr@gmail.com>
2024-03-05 23:05:21 +0300

    1 and 3 week

---
+++ Assets/Scripts/Fireball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireball : MonoBehaviour
+{
+    public float Speed;
+    public float Lifetime;
+    public float damage = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("DestroyFireball", Lifetime);
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+    void FixedUpdate()
+    {
+        MoveFixedUpdate();
+    }
+    private void MoveFixedUpdate()
+    {
+        transform.position += transform.forward*Speed*Time.fixedDeltaTime;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        EnemyDamage(collision);
+        DestroyFireball();  
+    }
+    private void DestroyFireball()
+    {
+        Destroy(gameObject);
+    }
+    private void EnemyDamage(Collision collision)
+    {
+        var EnemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+        if (EnemyHealth!=null)
+        {
+            EnemyHealth.DealDamage(damage);
+        }
+    }
+}
---
+++ Assets/Scripts/FireballCaster.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireballCaster : MonoBehaviour
+{
+    public Fireball fireballPrefab;
+    public Transform fireballSourceTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(fireballPrefab, fireballSourceTransform.position, fireballSourceTransform.rotation);
+        }
+    }
+}
